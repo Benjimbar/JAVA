6b45c3f5dc66bd530910e3867a923ce06bdcbffa
Benjamín <minjiba03@gmail.com>
2020-10-29 09:44:51 -0600

    Datos primitivos

--- HolaMundo/src/TitaniumBEN/holamundo/HolaMundo.java
+++ HolaMundo/src/TitaniumBEN/holamundo/HolaMundo.java
@@ -5,6 +5,25 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo :)");
+		
+	//DATOS PRIMITIVOS
+		//enteros
+		byte age = 22;
+		short year = 2020;
+		int user_id = 160057;
+		long ants = 7172873981728L;
+		
+		//float
+		float foot = 33.33F;
+		double money = 16729997.97698689;
+		
+		//Char
+		char genre = 'M';
+		
+		//Boolean
+		boolean isVisible = true;
+		
+		
 	}
 
 }
